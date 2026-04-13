6cffaf36f7f508ed6fdb2a8d29894ed4c952f86c
VexGlz <vexglz@gmail.com>
2026-04-12 19:08:35 -0700

    Diseño inicial del FrmPrincipal y paneles de usuario

---
+++ src/main/java/com/mycompany/presentacion_rizzmeup/Presentacion_RizzMeUp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.presentacion_rizzmeup;
+
+/**
+ *
+ * @author Erik
+ */
+public class Presentacion_RizzMeUp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
