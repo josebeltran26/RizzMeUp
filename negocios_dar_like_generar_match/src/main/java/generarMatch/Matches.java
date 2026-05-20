a9c2f73d4ae931741e1b402890c24d1a9c2fc981
josebeltran26 <jose.beltran262119@potros.itson.edu.mx>
2026-05-20 17:19:57 -0600

    merge: agregar archivo de rama Erick - negocios_dar_like_generar_match/src/main/java/generarMatch/Matches.java

---
+++ negocios_dar_like_generar_match/src/main/java/generarMatch/Matches.java
@@ -0,0 +1,9 @@
+package generarMatch;
+
+/**
+ * Clase heredada/deprecada. La funcionalidad actual se trasladó a FMatches y GestorMatch
+ * según el diseño de StarUML.
+ */
+public class Matches {
+    // Clase deprecada para evitar conflictos de compilación.
+}
