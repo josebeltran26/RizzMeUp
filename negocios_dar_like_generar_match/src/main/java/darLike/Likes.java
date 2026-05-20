740f4318c9c35eaa63deaab3d5260994829c08fc
josebeltran26 <jose.beltran262119@potros.itson.edu.mx>
2026-05-20 17:19:53 -0600

    merge: agregar archivo de rama Erick - negocios_dar_like_generar_match/src/main/java/darLike/Likes.java

---
+++ negocios_dar_like_generar_match/src/main/java/darLike/Likes.java
@@ -0,0 +1,9 @@
+package darLike;
+
+/**
+ * Clase heredada/deprecada. La funcionalidad actual se trasladó a FLikes y GestorLikes
+ * según el diseño de StarUML.
+ */
+public class Likes {
+    // Clase deprecada para evitar conflictos de compilación.
+}
